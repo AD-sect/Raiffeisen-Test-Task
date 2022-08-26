004ff3b31c74d0f3512c0d5da41aa814eeac4fdc
AD-sect <anandmitruk@ya.ru>
2022-08-26 17:46:32 +0300

    some more correction of comments

--- src/main/java/ru/dmirtuk/raiffeisentesttask/enums/Status.java
+++ src/main/java/ru/dmirtuk/raiffeisentesttask/enums/Status.java
@@ -2,7 +2,7 @@ package ru.dmirtuk.raiffeisentesttask.enums;
 
 /**
  *It is the enum of two conditions, in which user can exist.
- * It made for realisation of simple regulation of access to the game functionality.
+ * It made for realisation of simple regulation of access to the game url.
  */
 public enum Status {
     GAMER,
